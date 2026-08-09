9a28c45eabec111769c8598b397027053e7197eb
MD AKMAL HUSSAIN <akmalh4921@gmail.com>
2026-08-09 14:45:25 +0530

    upload all

---
+++ azurerm_network_interface/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_network_interface" "nic" {
+  for_each = var.nics
+
+  name                = each.value.name
+  resource_group_name = each.value.resource_group_name
+  location            = each.value.location
+
+
+  ip_configuration {
+    name                          = each.value.ip_config_name
+    subnet_id                     = data.azurerm_subnet.data_subnet[each.key].id
+    public_ip_address_id          = data.azurerm_public_ip.data_pip[each.key].id
+    private_ip_address_allocation = each.value.private_ip_address_allocation
+  }
+}
+
+
---
+++ azurerm_public_ip/terraform.tfvars
@@ -0,0 +1,15 @@
+pips = {
+  pip1 = {
+    name                = "pip-dev-07"
+    resource_group_name = "rg-dev-07"
+    location            = "central india"
+    allocation_method   = "Static"
+  }
+
+   pip2 = {
+    name                = "pip-dev-08"
+    resource_group_name = "rg-dev-07"
+    location            = "central india"
+    allocation_method   = "Static"
+  }
+}
