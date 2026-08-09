4b129018a04110090c8a7b66e43eb81b86a500f5
MD AKMAL HUSSAIN <akmalh4921@gmail.com>
2026-08-09 17:07:52 +0530

    variable updated

--- azurerm_storage_account/variable.tf
+++ azurerm_storage_account/variable.tf
@@ -1,9 +1,7 @@
 variable "stgs" {
   type = map(object({
     name                     = string
-    resource_group_name      = string
     location                 = string
-    account_tier             = string
-    account_replication_type = string
+   
   }))
 }
